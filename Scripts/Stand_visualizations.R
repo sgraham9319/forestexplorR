0ec5b0868127f209f81baa6a26c845d1e0270cc1
sgraham9319 <sg9319@my.bristol.ac.uk>
2019-01-17 16:36:56 -0800

    Calculated and plotted UTMs for individual trees

--- Scripts/Stand_visualizations.R
+++ Scripts/Stand_visualizations.R
@@ -43,3 +43,57 @@ new_stand_UTM$y <- as.vector(st_coordinates(new_stand_UTM)[,2])
 
 # Plot stands
 mapview(new_stand_UTM)
+
+#===============================================
+# Obtaining UTM coordinates for individual trees
+#===============================================
+
+# Load 2013 mapping data
+mapping <- read.csv("../Data/Mapping_2013.csv")
+
+# Isolate AG05 stand
+ag05 <- mapping[mapping$StandID == "AG05", ]
+
+# Reload y azimuth data
+stand_azimuths <- read.csv("../Data/Stand_locations.csv")
+
+# Calculate degrees to radians conversion factor
+cf <- pi / 180
+
+# Extract y azimuth for AG05 and convert to radians
+y_azim <- stand_azimuths[stand_azimuths$standid == "AG05", "y_azim"]
+y_azim <- y_azim * cf
+
+# Calculate relative azimuths (in radians) from stand origin to each tree
+ag05$rel_az <- y_azim + atan(ag05$Xcoord / ag05$Ycoord)
+
+# Calculate distances from origin to each tree
+ag05$dists <- sqrt((ag05$Xcoord ^ 2) + (ag05$Ycoord ^ 2))
+
+# Extract rows with relative azimuths <= 90 degrees
+sub_90 <- ag05[(ag05$rel_az / cf) <= 90, ]
+super_90 <- ag05[(ag05$rel_az / cf) > 90, ]
+
+# Extract X and Y UTM coordinates of stand origin
+origin_x <- new_stand_UTM[new_stand_UTM$standid == "AG05", "x"][[1]]
+origin_y <- new_stand_UTM[new_stand_UTM$standid == "AG05", "y"][[1]]
+
+# Calculate UTM X and Y for sub 90 group
+sub_90$x_UTM <- origin_x + (sin(sub_90$rel_az) * sub_90$dists)
+sub_90$y_UTM <- origin_y + (cos(sub_90$rel_az) * sub_90$dists)
+
+# Calculate UTM X and Y for sub 90 group
+super_90$x_UTM <- origin_x + (sin(super_90$rel_az) * super_90$dists)
+super_90$y_UTM <- origin_y - (cos(super_90$rel_az) * super_90$dists)
+
+# Recombine two groups
+ag05_UTM <- rbind(sub_90, super_90)
+
+# Transform to spatial object
+coordinates(ag05_UTM) <- ~ x_UTM + y_UTM
+
+# Assign UTM Zone 10 to data
+proj4string(ag05_UTM) <- CRS("+init=epsg:32610")
+
+# Plot trees
+mapview(ag05_UTM)

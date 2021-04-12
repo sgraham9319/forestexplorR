cffa98c74d94892e492eec909fc97a06fc3bd0e1
sgraham9319 <sgraham3@uw.edu>
2021-04-12 13:39:53 +0200

    Created individual files for each user function

---
+++ R/growth_summary.R
@@ -0,0 +1,25 @@
+#======================================================
+# Calculate annual growth over entire measurment period
+#======================================================
+
+# Function requires a data frame where separate measurements of the same tree
+# appear in different rows. The data frame needs to have a column called "treeid"
+# that contains unique tree ID values, a column named "year" containing the year
+# of the measurement, and a column named "dbh" containing the dbh measurements.
+
+growth_summary <- function(data){
+  data %>% 
+    group_by(tree_id) %>% 
+    arrange(year) %>%
+    summarize(
+      stand_id = stand_id[1],
+      species = species[1],
+      first_record = year[1],
+      last_record = year[n()],
+      begin_size = dbh[1],
+      mean_size = mean(dbh),
+      midpoint_size = (min(dbh) + max(dbh)) / 2, 
+      annual_growth = (dbh[n()] - dbh[1]) / (year[n()] - year[1])
+    ) %>% 
+    mutate(size_corr_growth = sqrt(annual_growth / begin_size))
+}

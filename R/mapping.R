e7a7b69cb3a6edbd3816704d5b7809a35d03c713
sgraham9319 <sgraham3@uw.edu>
2021-06-18 10:27:10 +0100

    Updated data documentation to reflect removal of columns

--- R/mapping.R
+++ R/mapping.R
@@ -5,21 +5,17 @@
 #' part of the Pacific Northwest Permanent Sample Plot Network 
 #' (\url{http://pnwpsp.forestry.oregonstate.edu/})
 #' 
-#' @format A data frame with 9155 rows and 11 variables:
+#' @format A data frame with 9155 rows and 8 variables:
 #' \describe{
 #'   \item{tree_id}{unique identification code of the tree}
 #'   \item{stand_id}{name of plot in which tree is located}
-#'   \item{plot}{subplot in which tree is located}
 #'   \item{tag}{unique tag number of the tree}
 #'   \item{species}{species identity of tree as a four letter code}
 #'   \item{year}{year when mapping data was collected}
-#'   \item{tree_status}{code defining tree health condition}
 #'   \item{dbh}{diameter at breast height of the tree at the time of
 #'   measurement, in cm}
 #'   \item{x_coord}{within-plot x coordinate of tree, in meters from origin}
 #'   \item{y_coord}{within-plot y coordinate of tree, in meters from origin}
-#'   \item{size_cat}{size category of tree, regular if dbh > 15 but small if
-#'   dbh between 5 and 15}
 #' }
 #' @source Data are available from the Pacific Northwest Permanent Sample Plot
 #' Network on request \url{http://pnwpsp.forestry.oregonstate.edu/data}

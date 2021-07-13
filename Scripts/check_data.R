00fc732414f2cdef1d99d874fc97dbc0eb1de05b
sgraham9319 <sgraham3@uw.edu>
2021-07-13 11:47:42 -0700

    Added and documented messy tree dataset

--- Scripts/check_data.R
+++ Scripts/check_data.R
@@ -28,14 +28,77 @@ map_old[sample(1:nrow(map_old), 10), "species"] <- NA
 write.csv(map_old, "Data/messy_mapping.csv", row.names = F)
 
 
-# User defined parameters
-map_data <- map_old
-#map_data <- map_old %>% select(-species)
-max_x <- 100
-max_y <- 100
-test <- mapping_check(map_old, 100, 100)
 
-messy_mapping <- read.csv("Data/messy_mapping.csv")
-messy_mapping <- messy_mapping %>%
-  select(-c(plot, tree_status))
-usethis::use_data(messy_mapping, overwrite = T)
+# Check tree data
+
+#----------------------------
+# Create some messy tree data
+#----------------------------
+
+# Load cleaned tree data
+messy_tree <- tree
+
+# Subset to two stands to reduce dataset size
+messy_tree <- messy_tree %>%
+  filter(stand_id %in% c("AB08", "PP17"))
+
+# Duplicate some trees
+set.seed(100)
+dups <- messy_tree[sample(1:nrow(messy_tree), 10), ]
+
+# Make changes so all have errors in stand, species, or both
+dups$stand_id <- "AB08"
+dups$species <- "PSME"
+
+# Rejoin with other tree data
+messy_tree <- bind_rows(messy_tree, dups)
+
+# Create some trees without mapping data
+set.seed(500)
+no_map <- messy_tree[sample(1:nrow(messy_tree), 10), ]
+no_map$tree_id <- paste("TEST000", 0:9, sep = "")
+
+# Rejoin with other tree data
+messy_tree <- bind_rows(messy_tree, no_map)
+
+# Give some tree records an NA for dbh
+messy_tree[sample(1:nrow(messy_tree), 10), "dbh"] <- NA
+
+# Give some tree records an NA for stand_id
+messy_tree[sample(1:nrow(messy_tree), 10), "stand_id"] <- NA
+
+# Give some tree records an NA for species
+messy_tree[sample(1:nrow(messy_tree), 10), "species"] <- NA
+
+# Give some tree records an NA for year
+messy_tree[sample(1:nrow(messy_tree), 10), "year"] <- NA
+
+usethis::use_data(messy_tree)
+
+
+
+
+
+# Are required columns present? tree_id, stand_id, species, year, dbh
+
+# Tree ids that refer to multiple trees? i.e. duplicate ids
+
+# Trees without mapping data
+
+# Missing dbh data
+
+# Missing stand_id
+
+# Missing species
+
+# Missing year
+
+# Negative growth? No, check in growth calculation function only (add this to
+# documention of tree checking function)
+
+
+# Find trees that have any missing data and highlight them for investigation
+# No need to make the call on whether the missing data makes those trees 
+# totally unusable
+
+
